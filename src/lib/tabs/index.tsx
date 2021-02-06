686fd50fb64f0cc46ef69aedcd1006b28fc942d7
ian <1505811266@qq.com>
2021-02-06 16:06:53 +0800

    初始化仓库

---
+++ src/lib/tabs/index.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import "./style/index.scss";
+export default class Tabs extends Component{
+    render(){
+        return <div className="tabs">
+  tabs
+        </div>
+    }
+}
